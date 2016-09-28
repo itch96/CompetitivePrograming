e1dccb7d4e827e0754e97eaaa7b1449a07e64a34
Rahul Gupta <useritch96@gmail.com>
2016-09-28 11:55:07 +0530

    Solved

--- Beginner/09.sumsInATriangle.cpp
+++ Beginner/09.sumsInATriangle.cpp
@@ -1,7 +1,46 @@
 // Sums in a Triangle
 #include <iostream>
+#include <cstring>
+
+void findMaximumSum(int x[100][100], int n);
 
 int main(void) {
-	int t, n;
-	std::cout << "Enter the number of test cases";
+	int t, n, i = 0;
+
+	//std::cout << "Enter the number of test cases: ";
+	std::cin >> t;
+
+	for(i = 0; i < t; i ++) {
+	//	std::cout << "\nEnter the number of lines: ";
+		std::cin >> n;
+		int a[100][100] = {0};
+
+	//	std::cout << "\nEnter the data: \n";
+		for(int j = 0; j < n; j ++) {
+			for(int k = 0; k <= j; k ++) {
+				std::cin >> a[j][k];
+			}
+		}
+
+		findMaximumSum(a, n);
+		std::memset(a, 0, sizeof(a));
+
+	}
+
+	return 0;
+}
+
+void findMaximumSum(int x[100][100], int n) {
+	for(int i = (n - 2); i >= 0; i --) {
+		for(int j = 0; j <= i; j ++) {
+			int currentElement = x[i][j];
+			int justBelow = x[i + 1][j];
+			int belowRight = x[i + 1][j + 1];
+
+			if(currentElement + justBelow > currentElement + belowRight) { x[i][j] = currentElement + justBelow; }
+			else {x[i][j] = currentElement + belowRight; }
+		}
+	}
+
+	std::cout << x[0][0] << std::endl;
 }
