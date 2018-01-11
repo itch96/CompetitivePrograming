257e518288c58fecc3f24c1ac20babf18997fe1b
Rahul Gupta <useritch96@gmail.com>
2018-01-11 12:17:41 +0530

    Solved

--- Codechef/Beginner/ALTARAY.py
+++ Codechef/Beginner/ALTARAY.py
@@ -3,28 +3,22 @@ while T > 0 :
   T -= 1
   N = int(input())
   A = list(map(int, input().split(' ')))
-  output = ''
-  for i in range(0, N - 1) :
-    count = 1
-    sign = 0
-    if A[i] < 0 :
-      sign = 0
+  output = [1] * N
+  index = 0
+  
+  for i in range(N - 2, -1, -1) :
+    if A[i] * A[i + 1] < 0  :
+      # output.append(output[index] + 1)
+      # index += 1
+      output[i] = output[i + 1] + 1
     else :
-      sign = 1
-    for j in range(i + 1, N) :
-      if A[j] < 0 :
-        if sign == 1 :
-          count += 1
-        else :
-          break
-        sign = 0
-      elif A[j] > 0 :
-        if sign == 0 :
-          count += 1 
-        else :
-          break
-        sign = 1
-    output += str(count) + ' '
-    # print(count)
-  print(output + '1')
-  # print(1)
+      # output.append(1)
+      # index += 1
+      output[i] = 1
+
+  # result = ''
+  # for i in output :
+  #   result += str(i) + ' '
+
+  # print(result[len(result) - 2::-1])
+  print(*output)
